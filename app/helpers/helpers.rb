7605c17c696d50f6e741f343a935292077b8f3d1
Sophie Gu <sophieqgu@gmail.com>
2019-09-16 03:05:46 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,11 +2,11 @@ require_relative '../models/user'
 
 class Helpers
   def self.current_user(session)
-    @user = User.find(session[:id])
+    @user = User.find(session[:user_id])
   end 
   
   def self.is_logged_in?(session)
-    !!session[:id]
+    !!session[:user_id]
   end 
   
 end
